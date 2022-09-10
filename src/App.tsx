4884ba88cfd4fa910b5e5d0df0f4235dd2b0b9a2
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-09-10 11:21:33 +0900

    연휴시작전

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import {Home} from './pages/Home';
+import {About} from './pages/About';
+import {Login} from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/' element={<About />}></Route>
+          <Route path='/' element={<Login />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+type Props = {};
+
+export const About = (props: Props) => {
+  return <div>About</div>;
+};
