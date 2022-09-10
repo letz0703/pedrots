dd6e815d8301d9c2be27bc204938edc677720986
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-09-10 12:50:17 +0900

    manual folding 안되네...

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import {Home} from './pages/Home';
 import {About} from './pages/About';
 import {Login} from './pages/Login';
 
+// prev: create configure store.ts with reduxjs/toolkit @store.ts
+import {Provider} from 'react-redux';
+// todo: 뭘 provide 할 건데? wrap components allowing acess to states
+// todo: <Router>를 감싼다.
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/' element={<About />}></Route>
-          <Route path='/' element={<Login />}></Route>
-        </Routes>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/' element={<About />}></Route>
+            <Route path='/' element={<Login />}></Route>
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
---
+++ src/store.ts
@@ -0,0 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit';
+
+export const store = cofigureStore({
+  // gonna take reducer :https://youtu.be/yZqBVLcWSn8?t=296
+  // action & previous state then return new state
+  // it is a function how our states interact each other
+  // todo :App.tsx : import provider
+});
