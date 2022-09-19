bc4cf3bcd25faed2cf59bf5b54cdb8402b691685
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-09-19 12:04:28 +0900

    19월-2209 firebase 전

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from "react";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import {Home} from "./pages/Home";
+import {Navbar} from "./components/navbar";
 import {About} from "./pages/About";
 import {Login} from "./pages/Login";
 import {Provider} from "react-redux";
 import {store} from "./store";
+import {Main} from "./pages/main";
+import "./App.css";
 
 function App() {
   return (
     <div className='App'>
       <Provider store={store}>
         <Router>
-          <Link to='/'>homepage</Link>
-          <Link to='/login'>login</Link>
-          <Link to='/contact'>contact</Link>
+          <Navbar />
           <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/home' element={<Home />}></Route>
-            <Route path='/about' element={<About />}></Route>
-            <Route path='/login' element={<Login />}></Route>
+            <Route path='/' element={<Main />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/about' element={<About />} />
           </Routes>
         </Router>
       </Provider>
---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+import styles from "./navbar.module.css";
+
+type Props = {};
+
+export const Navbar = (props: Props) => {
+  return (
+    <div className={styles.nav}>
+      <Link to='/'>homepage</Link>
+      <Link to='/login'>login</Link>
+      <Link to='/contact'>contact</Link>
+    </div>
+  );
+};
