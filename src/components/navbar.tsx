a0c7bf71f1a1e584df5a2a302a9864d8d0f5ea83
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-09-23 11:17:59 +0900

    Fix null image problem

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,24 @@
 import {Link} from "react-router-dom";
 import styles from "./navbar.module.css";
+import {auth} from "../ts/firebase";
 
 type Props = {};
 
 export const Navbar = (props: Props) => {
+  const displayUserName = auth.currentUser?.displayName;
+  const displayUserPhoto = auth.currentUser?.photoURL;
+
   return (
-    <div className={styles.nav}>
-      <Link to='/'>home</Link>
-      <Link to='/login'>login</Link>
-      <Link to='/contact'>contact</Link>
-    </div>
+    <>
+      <div className={styles.nav}>
+        <Link to='/'>home</Link>
+        <Link to='/login'>login</Link>
+        <Link to='/contact'>contact</Link>
+      </div>
+      <div>
+        <p>{displayUserName}</p>
+        <img src={displayUserPhoto || ""} />
+      </div>
+    </>
   );
 };
