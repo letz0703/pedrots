b900c07906d131b15de9fc690eef28e826b3f1cf
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-09-26 11:47:20 +0900

    remove about

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,10 +14,8 @@ export const Navbar = (props: Props) => {
       <div className={styles.nav}>
         <Link to='/'>home</Link>
         <Link to='/login'>login</Link>
-        <Link to='/contact'>contact</Link>
       </div>
       <div>
-        {/* <p>{displayUserName}</p> */}
         <p>{user?.displayName}</p>
         <img src={user?.photoURL || ""} width='100' height='100' />
       </div>
