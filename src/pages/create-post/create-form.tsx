99f7d6dce8b91f55551e4267f6a4436a1c327c84
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-10-01 11:08:12 +0900

    yup  validation resolover

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {useForm} from "react-hook-form";
 import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
 //import styles from './create-form.module.css'
 
 export const CreateForm = () => {
@@ -8,15 +9,20 @@ export const CreateForm = () => {
     title: yup.string().required("Error Message"),
     description: yup.string().required("Error Message")
   });
+
+  const res_schema_into_yupResolver = yupResolver(schema);
+  const {} = useForm({resolver: res_schema_into_yupResolver});
   const myStyle = {
     backgroundColor: "orange"
   };
   return (
     <>
       <h1 style={myStyle}>CreateForm</h1>
-      <input placeholder='Title...' />
-      <textarea placeholder='Description...' />
-      <input type='submit' />
+      <form>
+        <input placeholder='Title...' />
+        <textarea placeholder='Description...' />
+        <input type='submit' />
+      </form>
     </>
   );
 };
