c2a1c1b48e83a00e8ccfbd1498479948dc50652f
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-10-08 12:18:56 +0900

    firestore FS addDoc

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -5,8 +5,11 @@ import {yupResolver} from "@hookform/resolvers/yup";
 //import styles from './create-form.module.css'
 import {addDoc, collection} from "firebase/firestore";
 import {db} from "../../ts/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth} from "../../ts/firebase";
 
 export const CreateForm = () => {
+  const [user] = useAuthState(auth);
   const schema = yup.object().shape({
     title: yup.string().required("Need Title"),
     description: yup.string().required("Descritption")
@@ -25,7 +28,15 @@ export const CreateForm = () => {
 
   const onCreatePost = (data: createFormData) => {
     // console.log(data);
-    addDoc(postRef, {title: data.title, description: data.description});
+    addDoc(
+      postRef, //
+      {
+        title: data.title,
+        description: data.description,
+        username: user?.displayName,
+        userId: user?.uid
+      }
+    );
   };
 
   const postRef = collection(db, "posts"); // db = getFirestore(app)
