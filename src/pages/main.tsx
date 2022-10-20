137fe83ced6ad21eaed884667091fb0cb09096c2
ManI <letz0703@gmail.com>
2022-10-21 04:03:22 +0900

    Loop thru postList

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,27 +1,29 @@
-import {useState} from "react";
-import {getDocs, collection} from "firebase/firestore/lite";
-import {db} from "../ts/firebase";
+import {useEffect, useState} from "react"
+import {getDocs, collection} from "firebase/firestore/lite"
+import {db} from "../ts/firebase"
 
 interface Post {
-  id: string;
-  userId: string;
-  title: string;
-  username: string;
-  description: string;
+  id: string
+  userId: string
+  title: string
+  username: string
+  description: string
 }
 export const Main = () => {
-  const [postList, setPostList] = useState<Post[] | null>(null);
-  const postRef = collection(db, "posts");
+  const [postList, setPostList] = useState<Post[] | null>(null)
+  const postRef = collection(db, "posts")
 
-  const getDocs_Posts = async () => {
-    const result = await getDocs(postRef);
+  const getDocs_postRef = async () => {
+    const result = await getDocs(postRef)
     // console.log(result.docs.map((row) => ({...row.data(), id: row.id})));
-    setPostList(result.docs.map((row) => ({...row.data(), id: row.id})) as Post[]);
-  };
+    setPostList(result.docs.map((row) => ({...row.data(), id: row.id})) as Post[])
+  }
   // XXX: PEDRO 17월-2210
   // https://youtu.be/skstYc-K6ws?list=PLpPqplz6dKxW5ZfERUPoYTtNUNvrEebAR&t=619
 
-  getDocs_Posts();
+  useEffect(() => {
+    getDocs_postRef()
+  }, [])
 
-  return <div>Home Page</div>;
-};
+  return <div>Home Page</div> // TODO: Loop thru postList
+}
